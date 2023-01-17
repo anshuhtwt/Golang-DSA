0359e581d2aeb4b12d2a1f25f07230f28a60a1f0
Anshu Kumar <95365748+anshuhtwt@users.noreply.github.com>
2023-01-18 01:36:06 +0530

    Update LinearSearch.go

--- Codes/LinearSearch.go
+++ Codes/LinearSearch.go
@@ -1,3 +1,5 @@
+#Integers
+
 package main
 
 import "fmt"
@@ -19,5 +21,33 @@ func main() {
 		fmt.Println("Target found at index:", index)
 	} else {
 		fmt.Println("Target not found in array")
+	}
+}
+
+***********************************************************************************************************************************************************
+
+#Strings
+
+package main
+
+import "fmt"
+
+func linearSearch(arr []string, target string) int {
+	for i, v := range arr {
+		if v == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	arr := []string{"apple", "banana", "cherry", "date"}
+	target := "cherry"
+	index := linearSearch(arr, target)
+	if index == -1 {
+		fmt.Println("Target not found in the array.")
+	} else {
+		fmt.Println("Target found at index: ", index)
 	}
 }
